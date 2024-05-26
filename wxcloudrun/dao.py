d30bcca94308ed7c84980e0ade78266d4ae66c57
ly <387800363@qq.com>
2024-05-26 21:24:58 +0800

    Changes

--- wxcloudrun/dao.py
+++ wxcloudrun/dao.py
@@ -3,7 +3,7 @@ import logging
 from sqlalchemy.exc import OperationalError
 
 from wxcloudrun import db
-from wxcloudrun.model import Counters
+from wxcloudrun.model import Counters, GreenUser
 
 # 初始化日志
 logger = logging.getLogger('log')
@@ -62,3 +62,43 @@ def update_counterbyid(counter):
         db.session.commit()
     except OperationalError as e:
         logger.info("update_counterbyid errorMsg= {} ".format(e))
+
+
+def insert_user(user):
+    """
+    插入一个user实体
+    :param user: GreenUser实体
+    """
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except OperationalError as e:
+        logger.info("insert_user errorMsg= {} ".format(e))
+
+
+def query_userbyid(user_id):
+    """
+    根据ID查询user实体
+    :param user_id: user的ID
+    :return: user实体
+    """
+    try:
+        return GreenUser.query.filter(GreenUser.user_id == user_id).first()
+    except OperationalError as e:
+        logger.info("query_userbyid errorMsg= {} ".format(e))
+        return None
+
+
+def update_user(user):
+    """
+    根据ID更新user的值
+    :param user实体
+    """
+    try:
+        user = query_userbyid(user.user_id)
+        if user is None:
+            return
+        db.session.flush()
+        db.session.commit()
+    except OperationalError as e:
+        logger.info("update_user errorMsg= {} ".format(e))
--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from wxcloudrun import db
-
+db.metadata.clear()  # 清空元数据
 
 # 计数表
 class Counters(db.Model):
@@ -13,3 +13,34 @@ class Counters(db.Model):
     count = db.Column(db.Integer, default=1)
     created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
     updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
+
+
+class GreenUser(db.Model):
+    __tablename__ = 'green_user_t'
+    __table_args__ = (
+        db.UniqueConstraint('user_id', name='uni_user_id'),
+        db.Index('idx_shop_id', 'shop_id'),
+    )
+    extend_existing = True
+    user_id = db.Column(db.String(300),primary_key=True, nullable=False, default='', comment='同openid')
+    _openid = db.Column(db.String(100), nullable=False, default='', comment='微信openid')
+    avatarUrl = db.Column(db.String(300), default='', comment='头像url')
+    created_date = db.Column(db.TIMESTAMP, nullable=False, default=datetime.now(), comment='创建时间')
+    phone_number = db.Column(db.String(20), nullable=False, default='', comment='手机号')
+    role =db.Column(db.String(20), nullable=False, default='', comment='角色（user:用户，admin：管理员）')
+    status = db.Column(db.SmallInteger, nullable=False, comment='审批状态，0：待审批，2：已审批')
+    user_name = db.Column(db.String(50), nullable=False, default='', comment='用户名，包括学生和老师')
+    shop_id = db.Column(db.SmallInteger, nullable=False, comment='门店id')
+    def __repr__(self):
+        return f"<GreenUser(user_id='{self.user_id}', _openid='{self._openid}', user_name='{self.user_name}')>"
+
+
+class GreenShop(db.Model):
+    __tablename__ = 'green_shop_t'
+    shop_id = db.Column(db.Integer, primary_key=True, autoincrement=True, comment='门店id')
+    shop_name = db.Column(db.String(200), default='', comment='门店名')
+    shop_address = db.Column(db.String(500), default='', comment='门店地址')
+
+    # 添加UNIQUE约束，尽管在SQL中已经定义了两个UNIQUE键，但在ORM中只需定义一次即可
+    __table_args__ = (db.UniqueConstraint('shop_id', name='uni_shop_id'), {'comment': '门店信息表'})
+
